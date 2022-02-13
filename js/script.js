d496e5f3499fdb2184ff6938f42ae9fc636a2594
jeni_dub <stanfordjeni@gmail.com>
2022-02-12 21:11:44 -0800

    Add code to retrieve a random image and display the photo and author credit on the website

--- js/script.js
+++ js/script.js
@@ -6,7 +6,24 @@ const img = document.querySelector(".img");
 const getImage = async function () {
     const res = await fetch ('https://picsum.photos/v2/list?limit=100');
     let images = await res.json();
-    console.log(images);
+    selectRandomImage(images);
 }
 
-getImage();
+const selectRandomImage = function(images) {
+    let randomIndex = Math.floor(Math.random() * images.length);
+    let randomImage = images[randomIndex];
+    console.log(randomImage);
+    displayImage(randomImage);
+}
+
+const displayImage = function(obj) {
+    const author = obj.author;
+    const imageAddress = obj["download_url"];
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+}
+
+button.addEventListener("click", function() {
+    getImage();
+});
